e78238704d5d7617e7fc7c5e63d3026cda1ec7fa
francescomerighi1202 <francescomerighi61@gmail.com>
2023-06-03 17:07:34 +0200

    Bug fix

--- src/Control/ControllerLoginMedico.java
+++ src/Control/ControllerLoginMedico.java
@@ -60,6 +60,8 @@ public class ControllerLoginMedico {
         passwordLabel.getStyleClass().add("password-label");
         usernameField.getStyleClass().add("username-field");
         passwordField.getStyleClass().add("password-field");
+        backButton.getStyleClass().add("back-button");
+        loginButton.getStyleClass().add("login-button");
     }
 
 }
