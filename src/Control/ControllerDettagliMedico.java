1a66b45cbcbd93ea45497bbb54d9bf52abbbdc1c
francescomerighi1202 <francescomerighi61@gmail.com>
2023-06-04 16:07:02 +0200

    Bug fix

--- src/Control/ControllerDettagliMedico.java
+++ src/Control/ControllerDettagliMedico.java
@@ -1,4 +1,16 @@
 package Control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ControllerDettagliMedico {
+
+    @FXML
+    private Label titoloDettagli;
+
+    @FXML
+    public void initialize() {
+        titoloDettagli.getStyleClass().add("details-title");
+    }
+
 }
