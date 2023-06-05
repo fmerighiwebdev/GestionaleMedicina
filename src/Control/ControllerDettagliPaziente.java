d220506bb06ee89c1abbb5bc1cbcffea112cfec5
francescomerighi1202 <francescomerighi61@gmail.com>
2023-06-05 15:48:47 +0200

    Agggiunta GUI PazienteDettagli.fxml

--- src/Control/ControllerDettagliPaziente.java
+++ src/Control/ControllerDettagliPaziente.java
@@ -1,4 +1,16 @@
 package Control;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ControllerDettagliPaziente {
+
+    @FXML
+    private TextField title;
+
+    public void initialize() {
+        title.getStyleClass().add("title");
+    }
+
 }
