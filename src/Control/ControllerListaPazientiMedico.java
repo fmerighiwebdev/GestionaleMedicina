caeb3ae69654e0ec3d44ec6e4e45f7bd9be54ee8
jz4ndo <matteozandonai47@gmail.com>
2023-06-06 16:51:43 +0200

    Aggiunta bottoni e dettagli

--- src/Control/ControllerListaPazientiMedico.java
+++ src/Control/ControllerListaPazientiMedico.java
@@ -9,9 +9,16 @@ public class ControllerListaPazientiMedico {
     private  TextField title;
     @FXML
     private TextField faketextfield1;
+    @FXML
+    private Button pazientiBottone;
+    @FXML
+    private Button logoutButton;
+
     public void initialize() {
         title.getStyleClass().add("title");
         faketextfield1.getStyleClass().add("faketextfield1");
+        pazientiBottone.getStyleClass().add("pazientiBottone");
+        logoutButton.getStyleClass().add("logout-button");
     }
 
 }
