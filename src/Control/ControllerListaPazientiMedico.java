86ec20a6b9bea76f8d2e0776a5a6a8ba372f0981
jz4ndo <matteozandonai47@gmail.com>
2023-06-06 16:56:38 +0200

    Aggiunta logout medico lista pazienti

--- src/Control/ControllerListaPazientiMedico.java
+++ src/Control/ControllerListaPazientiMedico.java
@@ -1,8 +1,13 @@
 package Control;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ControllerListaPazientiMedico {
     @FXML
@@ -13,6 +18,25 @@ public class ControllerListaPazientiMedico {
     private Button pazientiBottone;
     @FXML
     private Button logoutButton;
+
+    @FXML
+    private void bottoneLogout(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/MedicoLogin.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) logoutButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+            // Logout effettuato con successo
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Logout");
+            alert.setHeaderText(null);
+            alert.setContentText("Logout eseguito con successo");
+            alert.showAndWait();
+        } catch (Exception ePatientLogin) {
+            ePatientLogin.printStackTrace();
+        }
+    }
 
     public void initialize() {
         title.getStyleClass().add("title");
