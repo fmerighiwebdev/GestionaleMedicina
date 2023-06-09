5041c9a8addcca9fea4919b5647468ddab8d5a60
jz4ndo <matteozandonai47@gmail.com>
2023-06-09 17:31:42 +0200

    Altre modifiche

--- src/Control/ControllerDettagliMedico.java
+++ src/Control/ControllerDettagliMedico.java
@@ -1,17 +1,51 @@
 package Control;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class ControllerDettagliMedico {
 
     @FXML
-    private Label titoloDettagli;
+    private Button logoutButton;
 
+    @FXML
+    private Label fullName;
+
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @FXML
+    private void bottoneLogout(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/MedicoLogin.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) logoutButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+            // Logout effettuato con successo
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Logout: " + username);
+            alert.setHeaderText(null);
+            alert.setContentText("Logout eseguito con successo");
+            alert.showAndWait();
+        } catch (Exception ePatientLogin) {
+            ePatientLogin.printStackTrace();
+        }
+    }
     @FXML
     public void initialize() {
         // CSS Class
-        titoloDettagli.getStyleClass().add("details-title");
+        logoutButton.getStyleClass().add("logout-button");
     }
 
 }
